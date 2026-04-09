8732df3c4346779eb28e17e3b72a94ee2e6e2cfc
Shon Jimenez <sjmobilemediabiz@gmail.com>
2026-04-09 18:34:50 -0500

    Add Home component for HVAC services page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Heavy Air and Heat, Inc.</h1>
+      <p>Professional HVAC Services</p>
+      <p>Emergency: (555) 123-4567</p>
+    </div>
+  )
+}
