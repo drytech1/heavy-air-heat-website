48779ce614dc4d739592d85593f300421b3cc40c
Shon Jimenez <sjmobilemediabiz@gmail.com>
2026-04-20 12:14:14 -0500

    Remove unnecessary closing div tag

--- app/page.tsx
+++ app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
+      
 
       {/* Features Section */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
